05f0253385769190a3ac5f42d24c187c57235429
Nishka Ranadive <nishka.amol.aug2006@gmail.com>
2025-10-06 23:25:27 +0530

    Add files via upload

---
+++ exp-21_1.cpp
@@ -0,0 +1,40 @@
+//Name: Nishka Ranadive
+//PRN: 24070123082
+//Exp-21
+
+//Program-1
+#include <iostream>
+using namespace std;
+void selectionSort(int arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        int minIndex = i;
+        for (int j = i + 1; j < size; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        if (minIndex != i) {
+            swap(arr[i], arr[minIndex]);
+        }
+    }
+}
+int main() {
+    int size;
+    cout << "Enter the number of elements in the array: ";
+    cin >> size;
+    int arr[size];
+    cout << "Enter " << size << " elements: ";
+    for (int i = 0; i < size; i++) {
+        cin >> arr[i];
+    }
+
+    selectionSort(arr, size);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
